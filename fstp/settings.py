b0eff64a9389635bb5762e9eb25a4eaeebf273cb
Akshobya A S <asakshobya@gmail.com>
2026-05-29 01:17:08 +0530

    error in settings

--- fstp/settings.py
+++ fstp/settings.py
@@ -63,10 +63,10 @@ if DATABASE_URL:
     DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+            'default': dj_database_url.config(
+                default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+                conn_max_age=600,
+            )
     }
 
 AUTH_PASSWORD_VALIDATORS = [
